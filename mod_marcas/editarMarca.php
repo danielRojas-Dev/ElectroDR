cadc6028a468fd1de9f6e733e4b727efd22d0429
Marcos Franco <marcosfrc013@gmail.com>
2021-04-08 00:53:48 -0300

    Creano CRUD para Marcas

---
+++ mod_marcas/editarMarca.php
@@ -0,0 +1,46 @@
+<?php require_once '../layouts/head.php'; ?>
+
+
+<div class="container">
+	<h1 class="page-header text-center">Editar Frecuencia</h1>
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+			<?php 
+
+			require_once '../conexion/conexion.php';
+
+			$id = $_GET['id_marca'];
+
+			$sql = "SELECT * FROM marca WHERE id_marca = '$id'";
+
+			$result = $conexion->query($sql);
+
+
+			$row_upd = $result->fetch_assoc();
+			?>
+
+			<form method="POST" action="modificarMarca.php">
+
+				<input type="hidden" class="form-control" name="id" value="<?php echo $row_upd['id_marca']; ?>">
+
+				<div class="row form-group">
+					<div class="col-sm-12">
+						<label class="control-label modal-label">Editar Descripcion Marca:</label>
+					</div>
+					<div class="col-sm-12">
+						<input type="text" class="form-control" name="descrip_marca" required autofocus value="<?php echo $row_upd['descrip_marca']; ?>">
+					</div>
+				</div>
+
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger" onClick="location.href='index.php'">Cancelar</button>
+					<button type="submit" class="btn btn-warning">Modificar</a>
+					</form>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<?php require_once '../layouts/footer.php'; ?>
