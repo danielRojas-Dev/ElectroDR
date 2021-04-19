8b7181bafcca87a162033de849fa543bd48e11a1
Marcos Franco <marcosfrc013@gmail.com>
2021-04-19 15:22:47 -0300

    haciendo funcion click en el boton calcular de los productos para obtener el total del precio

--- assets/js/presupuesto.js
+++ assets/js/presupuesto.js
@@ -218,13 +218,33 @@ $(document).ready(function() {
 		}
 
 	});
+
+	// $('.btnProductosCalcular').on('click', function(event) {
+	// 	event.preventDefault();
+	// 	alert('dasd')
+	// });
 	$(document).on("click", ".btnProductosCalcular", function(e){
+		e.preventDefault();
+		let productoNombre = e.target.getAttribute("data-productosNombre");
+		let productoPrecio = e.target.getAttribute("data-productosPrecio");
 
-		// let productoNombre = $(this).data('productosPrecio');
-		// let productoPrecio = e.target.getAttribute("data-productosPrecio");
-
-		console.log(e)
+		let cantProducto = 0;
 		
+		while(true){
+			cantProducto = parseInt(prompt("Ingrese la cantidad de productos: "));
+
+			if(!isNaN(cantProducto) && cantProducto != null && cantProducto != "" && cantProducto > 0 && Number.isInteger(parseInt(cantProducto))){
+				break;
+			}else if(!cantProducto){
+				return;
+			}else{	
+				alert('Ingrese un numero valido para las cantidades del producto!');
+				continue;
+			}
+		}
+		
+		alert(`El precio total para la cantidad de ${cantProducto} productos de: ${productoNombre} es de: ${productoPrecio * cantProducto}$`);
+
 	});
 
 
