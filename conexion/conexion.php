ad5bfb13487e5372dca49e8270e28119399c8e32
daniel <danielrojas131415@gmail.com>
2022-03-11 11:01:56 -0300

    actualizacion del proyecto

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,15 +1,16 @@
-<?php
+<?php 
 
-$servidor = "localhost";
-$usuario = "root";
-$password = "";
-$base_de_datos = "electrodr";
+	$servidor = "localhost";
+	$usuario = "id16446890_electrodr_14";
+	$password = "DanielRojas_10";
+	$base_de_datos = "id16446890_electrodr_nueva";
 
-$conexion = new mysqli($servidor, $usuario, $password, $base_de_datos);
+	$conexion = new mysqli($servidor, $usuario, $password, $base_de_datos);
 
-if ($conexion->connect_errno) {
-	echo "No se puedo realizar la conexion: ("
+	if ($conexion->connect_errno) {
+		echo "No se puedo realizar la conexion: ("
 		. $conexion->connect_errno . ") " . $conexion->connect_errno;
-}
+	}
 
-$conexion->set_charset("utf8");
+	$conexion->set_charset("utf8");
+ ?>
